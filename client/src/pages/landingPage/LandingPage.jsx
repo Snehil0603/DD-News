5405b5933b30189635386f2a5f6bcf84dd64a90d
unknown <samayragupta7thapril@gmail.com>
2024-11-11 23:49:48 +0530

    alan-button

--- client/src/pages/landingPage/LandingPage.jsx
+++ client/src/pages/landingPage/LandingPage.jsx
@@ -16,33 +16,37 @@ export default function LandingPage() {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    alanBtn({
-      key: process.env.REACT_APP_ALAN_API_KEY,
-      onCommand: ({ command, articles, number }) => {
-        if (command === 'newHeadlines') {
-          setNewsArticles(articles);
-          setActiveArticle(-1);
-        } else if (command === 'instructions') {
-          setIsOpen(true);
-        } else if (command === 'highlight') {
-          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
-        } else if (command === 'open') {
-          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
-          const article = articles[parsedNumber - 1];
-
-          if (parsedNumber > articles.length) {
-            alanBtn().playText('Please try that again...');
-          } else if (article) {
-            window.open(article.url, '_blank');
-            alanBtn().playText('Opening...');
-          } else {
-            alanBtn().playText('Please try that again...');
+    if (process.env.REACT_APP_ALAN_API_KEY) {
+      alanBtn({
+        key: process.env.REACT_APP_ALAN_API_KEY,  // Initializes the Alan button with your API key
+        onCommand: ({ command, articles, number }) => {
+          if (command === 'newHeadlines') {
+            setNewsArticles(articles);
+            setActiveArticle(-1);
+          } else if (command === 'instructions') {
+            setIsOpen(true);
+          } else if (command === 'highlight') {
+            setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+          } else if (command === 'open') {
+            const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+            const article = articles[parsedNumber - 1];
+  
+            if (parsedNumber > articles.length) {
+              alanBtn().playText('Please try that again...');
+            } else if (article) {
+              window.open(article.url, '_blank');
+              alanBtn().playText('Opening...');
+            } else {
+              alanBtn().playText('Please try that again...');
+            }
           }
-        }
-      },
-    });
+        },
+      });
+    } else {
+      console.error("Alan AI key is missing.");
+    }
   }, []);
-
+  
   return (
     <div>
       <Navbar />
